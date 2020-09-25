7b2fc99317aeacbbd5f8b508d93ea11ac0537604
Mikhail-Lykov <Mikhail-Lykov@users.noreply.github.com>
2020-09-25 15:30:11 +0400

    Commit

---
+++ src/main/java/crud/model/Role.java
@@ -0,0 +1,45 @@
+package crud.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String role;
+
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public Role(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String getAuthority() {
+        return role;
+    }
+}
